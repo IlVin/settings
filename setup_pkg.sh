85c00c269ff1436802d38066862ed915ccf7459b
Ilia Vinokurov <ilvin@mail.ru>
2016-09-30 16:32:17 +0300

    123

--- setup_pkg.sh
+++ setup_pkg.sh
@@ -3,19 +3,27 @@
     sudo sed -i 's/^.*\/rabbitvcs\/ppa\/ubuntu.*$//g' /etc/apt/sources.list
 
     sudo apt-get update
-    sudo apt-get install exfat-fuse exfat-utils
-    sudo apt-get install pkg-config libtool automake
-    sudo apt-get install graphviz gv
-    sudo apt-get install pv
-    sudo apt-get install liblz4-tool
-    sudo apt-get install curl
-    sudo apt-get install vim-gtk
-    sudo apt-get install font-manager
-    sudo apt-get install git-svn
-    sudo apt-get install keychain
+    sudo apt-get install \
+        exfat-fuse \
+        exfat-utils \
+        pkg-config \
+        libtool \
+        automake \
+        graphviz \
+        gv \
+        pv \
+        parallel \
+        liblz4-tool \
+        curl \
+        htop \
+        sysstat \
+        vim-gtk \
+        font-manager \
+        subversion \
+        git-svn \
+        keychain \
+        clang \
 
-    sudo apt-get install clang
 
-
-    sudo update-alternatives --config c++
+        sudo update-alternatives --config c++
 
