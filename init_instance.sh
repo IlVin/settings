0d0f05b9e2bbe473233e779fec7fa5aa70fedcba
Ilia Vinokurov <ilvin@mail.ru>
2017-10-04 10:18:53 +0300

    Added init_instance script

---
+++ init_instance.sh
@@ -0,0 +1,10 @@
+# Скрипт инициализации вновь созданного инстанса
+
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install git -y
+git clone https://github.com/IlVin/settings.git ~/ilvin.git/
+
+sudo reboot now
+
+
