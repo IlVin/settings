f7224ae774a1b56dbc015af8ad54dc90a50f7a27
Ilia Vinokurov <ilvin@mail.ru>
2017-10-06 06:09:41 -0700

    Make setup_dev script

---
+++ setup_dev.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+sudo apt update
+
+# Configure utils
+sudo apt install -y pkg-config
+sudo apt install -y libtool
+sudo apt install -y automake
+sudo apt install -y graphviz
+
+# C++
+sudo apt install -y clang
+sudo update-alternatives --config c++
+
+# C++ libs
+sudo apt-get install -y libboost-all-dev
+sudo apt-get install -y libcurl4-openssl-dev
+sudo apt-get install -y libcurlpp-dev
+
+# For umbrello
+#sudo apt install -y kinit kio kio-extras kded5
+
