c331f77eb8ddaf15307d9798e692b3c0eef64792
Ilia Vinokurov <ilvin@mail.ru>
2017-10-18 09:30:40 -0700

    Minor fix

--- init_fs.sh
+++ init_fs.sh
@@ -23,9 +23,12 @@ do_disk_to_pool () # disk, vg_name, lv_names
         sudo lvcreate -V $SIZE -T $VG/$LVP -n $LV
         sudo mkfs -t ext4 /dev/$VG/$LV
         sudo sync /dev/$VG/$LV
+        sudo mkdir -p /mnt/$VG_NAME/$LV
+        sudo mkdir -p /mnt/snapshots/$VG_NAME/$LV
+        sudo sh -c "echo '/dev/$VG/$LV /mnt/$VG_NAME/$LV ext4 errors=remount-ro 0 0' >> /etc/fstab"
+        sudo mount /mnt/$VG_NAME/$LV
     done
 }
 
-do_disk_to_pool "/dev/sda" "data" "share dlna"
-do_disk_to_pool "/dev/sdb" "mirror" "share dlna"
-
+do_disk_to_pool "/dev/sda" "data" "homes share dlna backup"
+do_disk_to_pool "/dev/sdb" "mirror" "homes share dlna backup"
