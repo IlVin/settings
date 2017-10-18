378cb742e487dc3452abf4f5c081f02577210cdd
Ilia Vinokurov <ilvin@mail.ru>
2017-10-18 08:51:00 -0700

    Minor fixes

--- init_fs.sh
+++ init_fs.sh
@@ -1,33 +1,31 @@
 do_disk_to_pool () # disk, vg_name, lv_names
 {
     local DISK="$1"
-    local VG="$2"
-    local LVS="$3"
-    local SPPOOL="sp_$VG_NAME"
+    local VG_NAME="$2"
+    local LV_NAMES="$3"
 
-    echo "DISK=$DISK; VG=$VG; SPPOOL=$SPPOOL; LVS=$LVS"
+    local VG="vg_$VG_NAME"
+    local LVP="lvp_$VG_NAME"
 
     # Create phisical volumes
     sudo pvcreate $DISK
-    sudo sync $DISK
 
     # Create volume groups
     sudo vgcreate $VG $DISK
-    sudo sync $DISK
 
     # Create a thin pools
     SIZE=`sudo vgs -o vg_free_count --rows $VG | awk '{ print $2 }'`
 
-    sudo lvcreate -L $SIZE -T $VG/$SPPOOL
-    sudo sync $VG/$SPPOOL
+    sudo lvcreate -L $SIZE -T $VG/$LVP
 
-    for $LV in $LV_NAMES
+    for LV in $LV_NAMES
     do
-        sudo lvcreate -V $SIZE -T $VG/$SSPOOL -n $LV
+        sudo lvcreate -V $SIZE -T $VG/$LVP -n $LV
+        sudo mkfs -t ext4 /dev/$VG/$LV
         sudo sync /dev/$VG/$LV
     done
 }
 
-do_disk_to_pool("/dev/sda", "share", "share dlna")
-do_disk_to_pool("/dev/sdb", "mirror", "share dlna")
+do_disk_to_pool "/dev/sda" "data" "share dlna"
+do_disk_to_pool "/dev/sdb" "mirror" "share dlna"
 
