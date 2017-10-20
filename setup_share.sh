8dcc508653faf614c5ac77770d1981691b7a6772
Ilia Vinokurov <ilvin@mail.ru>
2017-10-20 14:03:45 +0300

    Bugfixes

--- setup_share.sh
+++ setup_share.sh
@@ -4,9 +4,16 @@ sudo apt update -y
 cd ~
 HOMEDIR=`pwd`
 
-sudo mkdir -p /mnt/data/dlna
-sudo mkdir -p /mnt/data/share
+# MNT
+sudo mkdir -p /mnt/mirror
+sudo mkdir -p /mnt/data
+sudo chown ilvin:ilvin -R /mnt/mirror
+sudo chown ilvin:ilvin -R /mnt/data
+
+# HOMES
+sudo mkdir -p /mnt/mirror/homes
 sudo mkdir -p /mnt/data/homes
+sudo mkdir -p /mnt/data/homes/logs
 sudo mkdir -p /mnt/data/homes/ilvin
 sudo mkdir -p /mnt/data/homes/maria
 sudo mkdir -p /mnt/data/homes/stephan
@@ -14,12 +21,16 @@ sudo mkdir -p /mnt/data/homes/sofia
 sudo mkdir -p /mnt/data/homes/peter
 sudo mkdir -p /mnt/data/homes/lidia
 sudo mkdir -p /mnt/data/homes/ivan
-sudo chown ilvin:ilvin -R /mnt/data/*
+sudo chown ilvin:ilvin -R /mnt/mirror/homes
+sudo chown ilvin:ilvin -R /mnt/data/homes
 
-sudo mkdir -p /mnt/mirror/dlna
+# SHARE
 sudo mkdir -p /mnt/mirror/share
-sudo mkdir -p /mnt/mirror/homes
-sudo chown ilvin:ilvin -R /mnt/mirror/*
+sudo mkdir -p /mnt/data/share
+sudo mkdir -p /mnt/data/share/logs
+sudo mkdir -p /mnt/data/share/data
+sudo chown ilvin:ilvin -R /mnt/mirror/share
+sudo chown ilvin:ilvin -R /mnt/data/share
 
 sudo useradd -d /dev/null -s /sbin/nologin maria
 sudo useradd -d /dev/null -s /sbin/nologin stephan
@@ -35,10 +46,15 @@ sudo rm -f /etc/samba/smb.conf
 sudo cp $HOMEDIR/ilvin.git/smb.conf /etc/samba/smb.conf
 sudo service smbd restart
 
+# DLNA
+sudo mkdir -p /mnt/mirror/dlna
+sudo mkdir -p /mnt/data/dlna
+sudo mkdir -p /mnt/data/dlna/logs
 sudo mkdir -p /mnt/data/dlna/videos
 sudo mkdir -p /mnt/data/dlna/pictures
 sudo mkdir -p /mnt/data/dlna/music
-sudo chown ilvin:ilvin -R /mnt/data/dlna/*
+sudo chown ilvin:ilvin -R /mnt/mirror/dlna
+sudo chown ilvin:ilvin -R /mnt/data/dlna
 
 sudo mkdir -p /mnt/data/dlna/cache
 sudo chown minidlna:minidlna -R /mnt/data/dlna/cache
