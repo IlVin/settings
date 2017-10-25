2c5700e11b07d221bd71fdd3914a52768e04b8bf
Ilia Vinokurov <ilvin@mail.ru>
2017-10-25 10:13:28 +0300

    Fixes

--- sync_scripts/share.sh
+++ sync_scripts/share.sh
@@ -1,14 +1,38 @@
-#!/bin/sh
-
+#!/bin/bash
 
 # требуется модуль dm-snapshot
 sudo modprobe dm-snapshot
 
-NAME="share"
 
-SRCPATH="/mnt/$NAME/"
+do_rsync () # snap_name, src_path, dst_path
+{
+    local SNAP_NAME=$1
+    local SRC_PATH=$2
+    local DST_PATH=$3
 
-DSTPATH="/mnt/mirror/$NAME/"
+    local SNAP_PATH="${SNAP_NAME}-autosnap"
+    local LOG_PATH="${SRC_PATH}/logs/${SNAP_NAME}_rsync.log"
+
+    local RSYNC_CMD="sudo nice -n 20 rsync --verbose --log-file=${LOG_PATH} --ignore-times --human-readable --inplace --copy-links --copy-dirlinks --perms --executability --xattrs --owner --group --times --recursive --exclude=lost+found --exclude=logs --delete --checksum ${SRC_PATH}/ ${DST_PATH}/"
+
+    SRC_LV_PATH=$(/bin/grep "${SRC_PATH}" /etc/fstab|/usr/bin/awk '{ print $1 }')
+    DST_LV_PATH=$(/bin/grep "${DST_PATH}" /etc/fstab|/usr/bin/awk '{ print $1 }')
+
+    echo "SRC_LV_PATH: ${SRC_LV_PATH}"
+    echo "DST_LV_PATH: ${DST_LV_PATH}"
+
+    echo "RSYNC: ${RSYNC_CMD}"
+
+    eval ${RSYNC_CMD}
+}
+
+NAME=`date -u +GMT-%Y.%m.%d-%H.%M.%S`
+
+do_rsync  ${NAME} "/mnt/data/share" "/mnt/mirror/share"
+do_rsync  ${NAME} "/mnt/data/homes" "/mnt/mirror/homes"
+do_rsync  ${NAME} "/mnt/data/dlna" "/mnt/mirror/dlna"
+
+exit
 DSTPOOL="/dev/mpool"
 DSTLVNAME="mirror_$NAME"
 DSTLVPATH="$DSTPOOL/$DSTLVNAME"
