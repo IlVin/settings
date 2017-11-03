b5732161a04e4963b23ff84f21027a23564297a8
Ilia Vinokurov <ilvin@mail.ru>
2017-11-03 10:48:18 +0300

    Added okular to install script

--- setup_gui.sh
+++ setup_gui.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+sudo apt install -y okular
 sudo apt install -y gimp
 sudo apt install -y libavformat-ffmpeg56 libavfilter-ffmpeg5 gstreamer1.0-libav
+sudo apt install -y vlc
 sudo apt purge   -y avahi-daemon
 
