c6bfeb1a631473e9b24dc116c91c03360c51e3eb
Ilia Vinokurov <ilvin@mail.ru>
2017-11-17 14:38:22 +0300

    Added JDK install cmds

--- setup_dev.sh
+++ setup_dev.sh
@@ -7,6 +7,15 @@ sudo apt install -y pkg-config
 sudo apt install -y libtool
 sudo apt install -y automake
 
+# Java
+echo oracle-java9-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+sudo add-apt-repository -y ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install -y oracle-java9-installer
+# If you want to make Oracle Java 9 default
+sudo apt-get install -y oracle-java9-set-default
+# sudo apt-get install --no-install-recommends oracle-java9-installer
+
 # Soft
 sudo apt install -y graphviz
 
