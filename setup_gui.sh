221b6d7becd86b513ad7c92a1aa7ccf06859849d
Ilia Vinokurov <ilvin@mail.ru>
2018-03-06 12:51:13 +0200

    added Telegram and Dropbox setups

--- setup_gui.sh
+++ setup_gui.sh
@@ -6,3 +6,9 @@ sudo apt install -y libavformat-ffmpeg56 libavfilter-ffmpeg5 gstreamer1.0-libav
 sudo apt install -y vlc
 sudo apt purge   -y avahi-daemon
 
+# Telegram
+sudo add-apt-repository ppa:atareao/telegram
+sudo apt update
+sudo apt install -y telegram
+
+sudo apt install -y nautilus-dropbox
