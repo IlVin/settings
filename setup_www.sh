d0230d6ba519567efae10d5e2e937963b58bfdfa
Ilia Vinokurov <ilvin@mail.ru>
2018-03-15 22:49:12 +0300

    Added certbot to setup script

--- setup_www.sh
+++ setup_www.sh
@@ -1,6 +1,19 @@
 
 # Setup WWW soft
 
+# NGINX
+sudo apt update
+sudo apt install -y nginx
+
+# SSL
+sudo apt update
+sudo apt install -y software-properties-common
+sudo add-apt-repository ppa:certbot/certbot
+sudo apt update
+sudo apt install -y python-certbot-nginx 
+sudo certbot --nginx
+
+# Docker
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 sudo apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
 apt-cache policy docker-engine
