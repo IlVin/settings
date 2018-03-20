6afa2cef7aa69414fc26f8c6277b34bca78840c1
Ilia Vinokurov <ilvin@mail.ru>
2018-03-20 00:20:48 -0700

    Fix for XUbuntu

---
+++ setup_cloud.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Telegram
+sudo add-apt-repository ppa:atareao/telegram
+sudo apt update
+sudo apt install -y telegram
+
+# Dropbox
+sudo apt install -y nautilus-dropbox
+
--- setup_gui.sh
+++ setup_gui.sh
@@ -16,15 +16,8 @@ sudo apt install -y gimp
 sudo apt install -y libavformat-ffmpeg56 libavfilter-ffmpeg5 gstreamer1.0-libav
 sudo apt install -y vlc
 
-# Telegram
-#sudo add-apt-repository ppa:atareao/telegram
-#sudo apt update
-#sudo apt install -y telegram
-
-# Dropbox
-#sudo apt install -y nautilus-dropbox
-
 # Network Manager
 sudo apt install -y network-manager-openvpn
 sudo apt install -y network-manager-openvpn-gnome
 sudo service network-manager restart
+
