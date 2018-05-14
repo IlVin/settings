de2bb4dd6d0c23f51060e4e16e99ee8d95dd8503
Ilia Vinokurov <ilvin@mail.ru>
2018-05-14 10:55:06 +0300

    Added Remmina RDP

--- setup_gui.sh
+++ setup_gui.sh
@@ -16,6 +16,11 @@ sudo apt install -y gimp
 sudo apt install -y libavformat-ffmpeg56 libavfilter-ffmpeg5 gstreamer1.0-libav
 sudo apt install -y vlc
 
+# Remmina
+sudo apt-add-repository ppa:remmina-ppa-team/remmina-next
+sudo apt-get update
+sudo apt-get install remmina remmina-plugin-rdp
+
 # Network Manager
 sudo apt install -y network-manager-openvpn
 sudo apt install -y network-manager-openvpn-gnome
